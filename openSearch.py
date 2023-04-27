df24631824f0739f723495c5846d5876e1714b1e
Pedro Alejandro Pacheco Tripi <pedro.alejandro.pt@gmail.com>
2023-04-27 10:17:06 +0200

    Adding Arg Simple Validations

--- openSearch.py
+++ openSearch.py
@@ -2,19 +2,15 @@ from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
 import argparse
 import boto3
 import json
-  
 
- 
- 
 # Initialize parser
 parser = argparse.ArgumentParser()
 
-
 # Adding optional argument
-parser.add_argument("-d", "--domain", help = "Set Host")
+parser.add_argument("-p", "--port", help = "Set Port")
+parser.add_argument("-d", "--domain", help = "Set Domain")
 parser.add_argument("-r", "--region", help = "Set Region")
 parser.add_argument("-i", "--index", help = "Set Index")
-parser.add_argument("-p", "--port", help = "Set Port")
 parser.add_argument("-f", "--file", help = "JSON File with Query")
 parser.add_argument("-l", "--log", help = "Set Log Filename & Return it")
  
@@ -22,18 +18,29 @@ parser.add_argument("-l", "--log", help = "Set Log Filename & Return it")
 args = parser.parse_args()
 print(args)
 
+if args.domain == None:
+    print('You should provided a domain.\n')
+    exit()
+if args.port == None:
+    print('You should provided a port.\n')
+    exit()
+if args.index == None:
+    print('You should provided a index name.\n')
+    exit()
 if args.file == None:
     print('You should provided a json file.\n')
     exit()
+if args.region == None:
+    print('Using Default Region (us-west-1).\n')
 
 json_file_name = args.file
 # Cluster Endpoint, Example: my-test-domain.us-east-1.es.amazonaws.com
-host = args.domain if args.domain else 'my-test-domain.us-east-1.es.amazonaws.com' 
+host = args.domain
 # Port, Example: 443, 9443, ...
-port = args.port if args.port else 443
+port = args.port
 # Region, Example: us-west-1, us-west-2, eu-west-1, eu-west-2, ...
 region = args.region if args.region else 'eu-west-2'
-log_file_name = args.log if args.log else 'log'
+log_file_name = args.log
 
 credentials = boto3.Session().get_credentials()
 auth = AWSV4SignerAuth(credentials, region)
@@ -61,6 +68,7 @@ json_result = json.dumps(response, sort_keys=True, indent=4)
 print('Results:\n')
 print(json_result)
 
-f = open(log_file_name + '.txt', "w")
-f.write("{0}\n".format(json_result))
-f.close()
+if args.log == None:
+    f = open(log_file_name + '.txt', "w")
+    f.write("{0}\n".format(json_result))
+    f.close()
